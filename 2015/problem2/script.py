e2861a4a19a6693e403b606e164631afe83d99c2
Tommy Alpert <tommy0356@gmail.com>
2022-10-24 18:16:09 -0400

    distributive property

--- 2015/problem2/script.py
+++ 2015/problem2/script.py
@@ -11,7 +11,7 @@ def find_total_wrapping(inputArr):
         sorted_dimensions = sorted(present)
         side1, side2, side3 = sorted_dimensions
 
-        present_paper = 2*side1*side2 + 2*side1*side3 + 2*side2*side3
+        present_paper = 2*(side1*side2 + side1*side3 + side2*side3)
         present_paper += side1*side2
         total_paper += present_paper
     print(total_paper)
@@ -20,7 +20,7 @@ def find_total_ribbon(inputArr):
     total_ribbon = 0
     for present in inputArr:
         side1, side2, side3 = sorted(present)
-        wrap_ribbon = side1*2 + side2*2
+        wrap_ribbon = 2(side1 + side2)
         bow_ribbon = side1*side2*side3
         total_ribbon += wrap_ribbon + bow_ribbon
     print(total_ribbon)
