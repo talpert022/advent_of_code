8aa587ce9918b0c0d5fbfc17c016e9e3c794baca
Tommy Alpert <tommy0356@gmail.com>
2022-12-07 15:47:18 -0500

    2022 day1&2

---
+++ 2022/day1/script.py
@@ -0,0 +1,17 @@
+with open('input.txt') as txt:
+    input = txt.read()
+    elves = input.split('\n\n')
+
+# part 1
+
+topelf = float('-inf')
+for elf in elves:
+    cals = [int(cal) for cal in elf.splitlines()]
+    topelf = max(topelf, sum(cals))
+
+print(topelf)
+
+# part 2
+
+elf_cals = [sum([int(cal) for cal in elf.splitlines()]) for elf in elves]
+print(sum(sorted(elf_cals, reverse=True)[:3]))
