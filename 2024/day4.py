eda50832dff1aedcbb63d9a93cee750d69603a06
Tommy Alpert <tommy0356@gmail.com>
2024-12-19 13:18:09 -0500

    day 4 pt 2

--- 2024/day4.py
+++ 2024/day4.py
@@ -16,11 +16,11 @@ for r,_ in enumerate(letters):
         horizontally_fucked = c + SEGMENT_LENGTH > len(letters[r])
         vertically_fucked = r + SEGMENT_LENGTH > len(letters)
 
-        if not horizontally_fucked:
+        if not horizontally_fucked: 
             segments += [[letters[r][c+i] for i in range(SEGMENT_LENGTH)]]
         if not vertically_fucked:
             segments += [[letters[r+i][c] for i in range(SEGMENT_LENGTH)]]
-        if not horizontally_fucked and not vertically_fucked:
+        if not horizontally_fucked and not vertically_fucked: # diagonal
             segments += [[letters[r+i][c+i] for i in range(SEGMENT_LENGTH)]]
         if c >= SEGMENT_LENGTH-1 and not vertically_fucked: # reverse diagonal
             segments += [[letters[r+i][c-i] for i in range(SEGMENT_LENGTH)]]
@@ -29,3 +29,19 @@ count = len([seg for seg in segments if ''.join(seg) in ["XMAS", "SAMX"]])
 print(count)
 
 # PART 2
+
+crossed_mas = 0
+for r in range(1, len(letters)-1):
+    for c in range(1, len(letters[r])-1):
+        if letters[r][c] != 'A':
+            continue
+
+        if {letters[r-1][c-1], letters[r+1][c+1]} != {'M', 'S'}:
+            continue
+
+        if {letters[r+1][c-1], letters[r-1][c+1]} != {'M', 'S'}:
+            continue
+        
+        crossed_mas += 1
+
+print(crossed_mas)
