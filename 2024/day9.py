04bf9f1bf488584f981f31e6dc0fa6049a1f245d
Tommy Alpert <51763119+talpert022@users.noreply.github.com>
2025-09-02 19:30:00 +0000

    finish day 9 pt 1

--- 2024/day9.py
+++ 2024/day9.py
@@ -6,8 +6,7 @@ Is there a way to calculate checksum without expanding the input
 
 - compact the disk
   - start and end pointers, swapping, and skipping free spaces/file blocks
-    - find last non empty space, find first empty space, swap
-
+    - find first empty space, find last non empty space, swap
 
 - calculate checksum -> easy
 
@@ -16,15 +15,25 @@ Is there a way to calculate checksum without expanding the input
 with open('input.txt') as f:
   diskmap = f.read()
 
-expmap, on_file = "", True
+expmap, on_file = [], True
 for idx, num in enumerate(diskmap):
-  file_char = str(idx // 2) if on_file else "."
-  exp_format = ''.join([file_char] * int(num))
-  expmap += exp_format
+  file_id = idx // 2 if on_file else -1
+  expmap.extend([file_id] * int(num))
   on_file = not on_file
 
 start, end = 0, len(expmap) - 1
 
 while end > start:
-  pass
+  if expmap[start] == -1 and expmap[end] != -1:
+    expmap[start], expmap[end] = expmap[end], expmap[start]
 
+  start += 1 if expmap[start] != -1 else 0
+  end -= 1 if expmap[end] == -1 else 0
+
+print(expmap[:50])
+checksum = 0
+for idx, file_val in enumerate(expmap):
+  if file_val == -1: break
+  checksum += int(file_val)*idx
+
+print(checksum)
